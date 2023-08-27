cf12ca352ca8f5fb71f18fdb2007d58841d6c18e
djun90 <49852363+djun90@users.noreply.github.com>
2023-08-27 10:21:57 +0800

    Add files via upload

--- 新闻早报.js
+++ 新闻早报.js
@@ -2,7 +2,7 @@
  * @author 小寒寒
  * @name 新闻早报
  * @origin 小寒寒
- * @version 1.0.0
+ * @version 1.0.1
  * @description 新闻早报，自行更改群号
  * @rule ^新闻早报$
  * @priority 1000
@@ -10,20 +10,44 @@
  * @cron 0 0 7 * * *
  * @disable false
  * */
-
+const img = 'https://api.caonm.net/api/60/index';
+const config = [
+    {
+        userId: 0,
+        groupId: 43205083856,
+        platform: 'wxQianxun'
+    },
+    {
+        userId: 0,
+        groupId: 368256161,
+        platform: 'qq'
+    },
+    {
+        userId: 'jun812148374',
+        groupId: 0,
+        platform: 'wxQianxun'
+    }
+]
 module.exports = async s => {
     if (await s.getFrom() == 'cron') {
-        await sysMethod.push({
-            platform: 'wxQianxun',
-            groupId: 43205083856,
-            path: 'https://api.caonm.net/api/60/index',
-            type: 'image'
-        })
+        for (let item of config || []) {
+            await push(item.userId, item.groupId, item.platform);
+        }
     }
     else {
         await s.reply({
-            path: 'https://api.caonm.net/api/60/index',
+            path: img,
             type: 'image'
         })
+    }
+
+    async function push(userId, groupId, platform) {
+        await sysMethod.push({
+            platform: platform,
+            userId: userId,
+            groupId: groupId,
+            path: img,
+            type: 'image'
+        });
     }
 }

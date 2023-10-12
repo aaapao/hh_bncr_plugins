8fc3e3040012657597280e27d9ef321bd4a84754
小寒寒 <49852363+djun90@users.noreply.github.com>
2023-10-12 20:32:45 +0800

    update v1.0.7

--- 青龙通知接口.js
+++ 青龙通知接口.js
@@ -2,7 +2,7 @@
  * @author 小寒寒
  * @name 青龙通知接口
  * @origin 小寒寒
- * @version 1.0.6
+ * @version 1.0.7
  * @description 青龙通知接口，根据通知标记活动，适配于麦基EVE库，搭配库里SpyIsValid使用，需配置对接token，set SpyIsValid ql_token xxxx，自行需要修改推送群号 不能其它通知接口插件共用
  * @public false
  * @priority 99
@@ -13,6 +13,7 @@
  * 1.0.4 适配积分兑换，增加更多垃圾活动标记，优化日期处理
  * 1.0.5 修复bug
  * 1.0.6 优化及调整
+ * 1.0.7 协助作者收集高质量活动店铺ID 80-94
  */
 
 // 指定标题推送
@@ -42,6 +43,7 @@ const push3 = {
 
 const SpyIsValid = new BncrDB('SpyIsValid');
 const dayjs = require('dayjs');
+const request = require('util').promisify(require('request'));
 /* post接口 */
 router.post('/api/qinglongMessage', async (req, res) => {
     try {
@@ -74,6 +76,20 @@ router.post('/api/qinglongMessage', async (req, res) => {
                 msg: `${title}\n\n${message}`,
                 type: 'text'
             });
+
+            // 协助作者收集高质量活动店铺ID
+            try {
+                let shopInfo = /店铺信息:[\d]+_[\d]+/.exec(message)?.toString() || '';
+                // console.log(shopInfo);
+                if (shopInfo) {
+                    let shopId = shopInfo.split(':')[1];
+                    /** Code Encryption Block[419fd178b7a37c9eae7b7426c4a04203c90f097f761598b157b007255f983ec5b7989e0d1f6512c02a61c55314733c8762798fad21dd0f2c79bd82ec3ec11145d92cdb7775649987daf569d41a6c9c91f1c3dd85cbbecbabf55dfcc44ace4b99150952dda785b4c6aaf2a9286dced20b704e3cfee00cdf4a8218092119e33d097124b62866be8ee265bc0d49d1a5a2948e258dc0bd397fed47eee22c39552d723ff633ff7fcbe8d4c84952a1203f62ba] */
+                    message = message.replace(shopInfo, shopInfo + '(店铺ID已收集)')
+                }
+            }
+            catch(e){ 
+                console.log(e)
+            }
         }
 
         if (message.includes('export ') && title[0] == 'M') {

3c0e1809ef852fa69179827111c50dc708bb2662
小寒寒 <49852363+djun90@users.noreply.github.com>
2023-10-30 00:14:08 +0800

    Add files via upload

--- 短链解析.js
+++ 短链解析.js
@@ -2,12 +2,12 @@
  * @author 小寒寒
  * @name 短链解析
  * @origin 小寒寒
- * @version 1.0.1
+ * @version 1.0.2
  * @description 短链解析
  * @rule https?:\/\/jd\.lsy22\.cn\/[0-9A-Za-z]{3,8}
  * @rule https?:\/\/t\.cn\/[0-9A-Za-z]{3,8}
  * @rule https?:\/\/jx\.gixiu\.com\/[0-9A-Za-z]{3,8}
- * @rule https?:\/\/kzurl16\.cn\/[0-9A-Za-z]{3,8}
+ * @rule https?:\/\/kzurl\d{2}\.cn\/[0-9A-Za-z]{3,8}
  * @rule https?:\/\/u\.jd\.lu1dou\.com\/[0-9A-Za-z]{3,8}
  * @rule https:\/\/u\.jd\.com\/[0-9a-zA-Z]{7}
  * @rule https?:\/\/sourl\.cn/[0-9a-zA-Z]{3,8}
@@ -26,63 +26,73 @@ https://u.jd.lu1dou.com  示例：https://u.jd.lu1dou.com/i3rx
 */
 
 // 监听的群号
-const whiteGroups = ['-1001804013199']
+const whiteGroups = ['-1001804013199'];
 
 const request = require('util').promisify(require('request'));
+const SpyHandleMsg = require('../红灯区/mod/SpyHandleMsg');
+
 module.exports = async s => {
     const content = await s.getMsg();
-    if ((!whiteGroups?.includes(await s.getGroupId()) && !(await s.isAdmin())) || /^(set) ([^ \n]+) ([^ \n]+) ([\s\S]+)$/.test(content)) return 'next';
+    const isAdmin = await s.isAdmin();
+    if ((!whiteGroups?.includes(await s.getGroupId()) && !isAdmin) || /^(set) ([^ \n]+) ([^ \n]+) ([\s\S]+)$/.test(content)) return 'next';
     const ujds = content.match(/https?:\/\/[A-Za-z0-9\-\._~:\/\?#\[\]@!$&'\*\+,%;\=]*/g);
     if (ujds[0]) {
+        let jx = [];
         for (let ujd of ujds || []) {
             try {
+                let commond = ujd;
                 // console.log(ujd)
-                let res = await request({
-                    url: ujd,
-                    method: 'get',
-                    followRedirect: false,
-                    timeout: 3000
-                });
-                let data = res.body;
-                let url = res.headers.location;
-                let commond = '';
-                if (res.body && /u\.jd\.com/.test(ujd)) {
-                    let jdurl = data.match(/(https:\/\/u\.jd\.com\/jda[^']+)/) && data.match(/(https:\/\/u\.jd\.com\/jda[^']+)/)[1] || ''
-                    // console.log(url);
-                    if (jdurl) {
-                        let res2 = await request({
-                            url: jdurl,
-                            method: 'get',
-                            followRedirect: false
-                        });
-                        url = res2.headers.location;
+                if (/https?:\/\/[a-z0-9A-Z\.]+\/[0-9a-zA-Z]{3,8}/.test(ujd)) {
+                    let res = await request({
+                        url: ujd,
+                        method: 'get',
+                        followRedirect: false,
+                        timeout: 3000
+                    });
+                    let data = res.body;
+                    let url = res.headers.location;
+                    if (res.body && /u\.jd\.com/.test(ujd)) {
+                        let jdurl = data.match(/(https:\/\/u\.jd\.com\/jda[^']+)/) && data.match(/(https:\/\/u\.jd\.com\/jda[^']+)/)[1] || ''
+                        // console.log(url);
+                        if (jdurl) {
+                            let res2 = await request({
+                                url: jdurl,
+                                method: 'get',
+                                followRedirect: false
+                            });
+                            url = res2.headers.location;
 
-                        // 关注有礼
-                        if (/shopId=[\d]+/.test(url) && /关注/.test(content)) {
-                            commond = `export M_FOLLOW_SHOP_ARGV="${url}"`;
-                        }
-                        // 左侧刮奖
-                        else if (/shopId=[\d]+/.test(url) && /左.*?刮/.test(content)) {
-                            commond = `export M_GYG_SHOP_ARGV="${url}"`;
-                        }
-                        else {
-                            commond = url;
+                            // 关注有礼
+                            if (/shopId=[\d]+/.test(url) && /关注/.test(content)) {
+                                commond = `export M_FOLLOW_SHOP_ARGV="${url}"`;
+                            }
+                            // 左侧刮奖
+                            else if (/shopId=[\d]+/.test(url) && /左.*?刮/.test(content)) {
+                                commond = `export M_GYG_SHOP_ARGV="${url}"`;
+                            }
+                            else {
+                                commond = url;
+                            }
                         }
                     }
+                    else {
+                        commond = url || ujd;
+                    }
                 }
-                else {
-                    commond = url;
-                }
-                if (commond) {
-                    console.log(commond)
-                    await s.inlineSugar(commond);
-                }
+                commond = commond.replace(/\s/, '');
+                let spy = await SpyHandleMsg(commond);
+                jx.push(spy || commond);
             }
             catch (e) {
                 console.log(e);
                 continue;
             }
         }
+        let msg = jx.join('\n┄┅┄┅┄┅┄┅┄┅┄┅┄\n');
+        if (msg && isAdmin) {
+            await s.reply(`短链解析结果：\n${msg}`);
+        }
+        return msg ? await s.inlineSugar(msg) : 'next';
     }
     return 'next';
 }
